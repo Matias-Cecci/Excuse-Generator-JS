43833db5ab33e61b2693bee37f7d9e1a59bab3d7
Matías Exequiel Cecci <matias_cecci@hotmail.com>
2022-10-07 09:23:52 +0200

    Generador de excusas listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['El perro', 'Mi Abuela', 'Messi', 'Su primo', 'Su gato', 'Un avión']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let what = ['se enfermó', 'se cayó', 'se quebró', 'se perdió', 'se durmió', 'desapareció' ]
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let when = ['durante la cena', 'en el tren', 'mientras yo estudiaba', 'en el supermercado', 'justo cuando terminé', 'durante su cumpleaños']
+
+function excuseGenerator(who, what, when){
+    numWho = Math.floor(Math.random() * who.length);
+    numWhat = Math.floor(Math.random() * what.length);
+    numWhen = Math.floor(Math.random() * when.length);
+    return who[numWho] + " " + what[numWhat] + " "  + " " + when[numWhen];
+}
+
+function onLoad (){
+    let excusa = document.getElementById('excuse');
+    excusa.innerHTML = excuseGenerator(who, what, when)
+
+}
+
+window.onload = onLoad;
